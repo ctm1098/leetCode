522c16716e774052db959198e85911c6148981d0
ctm1098 <carlosbond1998@gmail.com>
2020-09-16 09:53:19 +0200

    Revert "method de nuevo"
    This reverts commit 617f7852897a64d8f54d48cda85dc1c22296b5b6.

--- LeetCode/src/clases/Main1.java
+++ LeetCode/src/clases/Main1.java
@@ -2,17 +2,11 @@ package clases;
 
 public class Main1 {
 	
-	private static void method() {
-		System.out.println("Holaaaa");
-		return;
-	}
-	
 	public static void main(String[] args) {
 		System.out.println("Hola mundo amendado otra vez");
 		System.out.println("Nuevo printf");
 		System.out.println("Otra prueba");
 		System.out.println("Una segunda prueba");
-		method();
 		System.out.println("Añadido a la segunda prueba");
 		int x = 2 + 2;
 		System.out.println(x);
