50880268a142fd0c19652d2579bf30b083fc002a
ctm1098 <carlosbond1998@gmail.com>
2020-09-16 09:54:06 +0200

    method de nuevo con comentarios

--- LeetCode/src/clases/Main1.java
+++ LeetCode/src/clases/Main1.java
@@ -2,13 +2,22 @@ package clases;
 
 public class Main1 {
 	
+	private static void method() {
+		System.out.println("Holaaaa");
+		return;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hola mundo amendado otra vez");
 		System.out.println("Nuevo printf");
+		//comentario
 		System.out.println("Otra prueba");
 		System.out.println("Una segunda prueba");
+		method();
+		//comentario
 		System.out.println("Añadido a la segunda prueba");
 		int x = 2 + 2;
+		//comentario
 		System.out.println(x);
 	}
 }
