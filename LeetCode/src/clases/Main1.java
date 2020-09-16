9dbf05757758a2581a22e31d2f1dab998ae4483e
ctm1098 <carlosbond1998@gmail.com>
2020-09-16 09:36:22 +0200

    otra prueba

--- LeetCode/src/clases/Main1.java
+++ LeetCode/src/clases/Main1.java
@@ -5,5 +5,6 @@ public class Main1 {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo amendado otra vez");
 		System.out.println("Nuevo printf");
+		System.out.println("Otra prueba");
 	}
 }
