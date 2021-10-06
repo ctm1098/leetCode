475b66422cab9471487146421c9d784bd8982520
ctm1098 <carlosbond1998@gmail.com>
2021-10-06 17:23:03 +0200

    bla

---
+++ ejercicios/src/jumpgame2/App.java
@@ -0,0 +1,11 @@
+package jumpgame2;
+
+public class App {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {9,8,2,2,0,2,2,0,4,1,5,7,9,6,6,0,6,5,0,5};
+        System.out.println(s.jump(arr));
+    }
+    
+}
